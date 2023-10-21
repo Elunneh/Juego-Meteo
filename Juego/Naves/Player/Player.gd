4ee18ed9a2ad40fdb55f8cbc5e191719a3b80193
Elune <lorena.salinas@gmail.comgit config --global user.email lorena.salinas@gmail.comgit config --global user.email lorena.salinas@gmail.com>
2023-10-21 17:22:43 -0300

    Update Player.gd

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,100 +1,124 @@
 class_name Player
 extends RigidBody2D
 
-enum ESTADO { SPAWN, VIVO, INVENCIBLE, MUERTO }
+##Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
 
-export var potencia_motor: int = 20
+##Atributos export
+export var potencia_motor:int = 20
 export var potencia_rotacion: int = 280
-export var estela_maxima: int = 150
+export var estela_maxima:int = 150
 
-var empuje: Vector2 = Vector2.ZERO
-var dir_rotacion: int = 0
-var estado_actual: int = ESTADO.SPAWN
 
-onready var canion: Canion = $Canion
-onready var laser: RayoLaser = $LaserBeam2D
-onready var estela: Estela = $EstelaPuntoInicio/Trail2D
+##Atributos
+var empuje:Vector2 = Vector2.ZERO
+var dir_rotacion:int = 0
+var estado_actual:int = ESTADO.SPAWN
+
+##Atributos Onready
+onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx: Motor = $MotorSFX
-onready var colisionador: CollisionShape2D = $CollisionShape2D
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
-func _ready() -> void:
+
+func _ready()-> void:
 	controlador_estados(estado_actual)
 
-func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+
+## Metodos
+func _integrate_forces(state: Physics2DDirectBodyState) ->void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
-func _process(delta: float) -> void:
+func _process(delta: float)-> void:
 	player_input()
-	print ("estare funcionando")
 
-func esta_input_activo() -> bool:
-	return estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]
-
-func _unhandled_input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent)-> void:
 	if not esta_input_activo():
 		return
+	
 
-	# Disparo de rayo
-	if event is InputEventAction:
-		if event.action == "disparo_secundario" and event.pressed:
-			laser.set_is_casting(true)
-		elif event.action == "disparo_secundario" and not event.pressed:
-			laser.set_is_casting(false)
+	
+	
+	
+	##disparo de rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+		
+##control de Estela y sonido de Motor
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+		
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(0)
+		motor_sfx.sonido_off()
+	if (event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
+		motor_sfx.sonido_off()
+		
 
-	# Control de estela y sonido de motor
-	if event is InputEventAction:
-		if event.action == "mover_adelante" and event.pressed:
-			estela.set_max_points(estela_maxima)
-			motor_sfx.sonido_on()
-		elif event.action == "mover_adelante" and not event.pressed:
-			estela.set_max_points(0)
-			motor_sfx.sonido_off()
 
-func controlador_estados(nuevo_estado: int) -> void:
-	if estado_actual != nuevo_estado:
-		estado_actual = nuevo_estado
-		match nuevo_estado:
-			ESTADO.SPAWN:
-				colisionador.set_deferred("disabled", true)
-				canion.set_puede_disparar(false)
-			ESTADO.VIVO:
-				colisionador.set_deferred("disabled", false)
-				canion.set_puede_disparar(true)
-			ESTADO.INVENCIBLE:
-				colisionador.set_deferred("disabled", true)
-			ESTADO.MUERTO:
-				colisionador.set_deferred("disabled", true)
-				canion.set_deferred("puede_disparar", false)
-				queue_free()
-			_:
-				printerr("Error de estado")
+## Metodos Custom
+
+
+func controlador_estados(nuevo_estado: int)-> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			queue_free()
+		_:
+			printerr("Error de Estado")
+	estado_actual = nuevo_estado
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	return true
 
 func player_input() -> void:
 	if not esta_input_activo():
 		return
-
-	# Empuje
+	
+	
+	#Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
-		empuje.x = potencia_motor
+		empuje = Vector2(potencia_motor, 0)
 	elif Input.is_action_pressed("mover_atras"):
-		empuje.x = -potencia_motor
-
-	# Rotación
+		empuje = Vector2(-potencia_motor, 0)
+		
+	#Rotacion
 	dir_rotacion = 0
-	if Input.is_action_pressed("rotar_antihorario"):
+	if Input. is_action_pressed("rotar_antihorario"):
 		dir_rotacion -= 1
-	if Input.is_action_pressed("rotar_horario"):
+	elif Input. is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
-
-	# Disparo
-	if Input.is_action_pressed("disparo_principal"):
+		
+##Disparo
+	if Input. is_action_pressed("disparo_principal"):
 		canion.set_esta_disparando(true)
-	elif Input.is_action_just_released("disparo_principal"):
+	if Input. is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
-		print("¡Estoy disparando!")
+		print("estoy disparando mama")
+		
 
-func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String)-> void:
 	if anim_name == "Spawn":
 		controlador_estados(ESTADO.VIVO)
